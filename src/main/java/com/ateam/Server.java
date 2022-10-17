baa29c45b60894419a894929f6682ccfb2548bce
Andrés Millán <andres.millan-munoz@t-systems.com>
2022-10-17 14:17:54 +0200

    Server should be ready

--- src/main/java/com/ateam/Server.java
+++ src/main/java/com/ateam/Server.java
@@ -2,13 +2,9 @@ package com.ateam;
 
 import java.io.*;
 import java.net.*;
-<<<<<<< HEAD
 import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.logging.Handler;
-=======
-import java.util.ArrayList;
->>>>>>> 8a669ec (WIP)
 
 
 /*
@@ -22,7 +18,6 @@ public class Server {
 
     ServerAccepClient newConnections;
     ServerSocket ss;
-
 
     /**
      * Entrypoint of the server.
