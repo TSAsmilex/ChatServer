7541ecfb262d663bed3e05c88476f37dd716a3f8
Andrés Millán <andres.millan-munoz@t-systems.com>
2022-10-17 12:46:44 +0200

    Server mockup

--- src/main/java/com/ateam/App.java
+++ src/main/java/com/ateam/App.java
@@ -4,10 +4,17 @@ package com.ateam;
  * Hello world!
  *
  */
-public class App 
+public class App
 {
     public static void main( String[] args )
     {
+        try {
+            Server.run(args);
+
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
         System.out.println( "Hello World!" );
     }
 }
