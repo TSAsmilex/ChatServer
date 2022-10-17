f30e442ad48da241249383d4a6f948cc0f02e772
Darthpaul0 <pablofr95@gmail.com>
2022-10-17 12:17:39 +0200

    Formatting

---
+++ src/main/java/com/ateam/ClientHandler.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.ateam;
+
+import java.net.Socket;
+import java.util.ArrayDeque;
+
+/**
+ *
+ * @author pferna12
+ */
+public class ClientHandler {
+
+    /**
+     *
+     * @param socket
+     */
+    public ClientHandler(Socket socket) {
+    }
+
+    /**
+     *
+     * @param myAwaitingQueue
+     */
+    public void awaitMessage() {
+    }
+}
---
+++ src/test/java/com/ateam/ClientHandlerTest.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit4TestClass.java to edit this template
+ */
+package com.ateam;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author pferna12
+ */
+public class ClientHandlerTest {
+    
+    public ClientHandlerTest() {
+    }
+
+    /**
+     * Test of awaitMessage method, of class ClientHandler.
+     */
+    @Test
+    public void testAwaitMessage() {
+        System.out.println("awaitMessage");
+        ClientHandler instance = null;
+        instance.awaitMessage();
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+}
